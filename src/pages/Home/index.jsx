adba7168a360de40debdbc9ee9c266fbe357ffdf
roberto-medeiros <robertomedeirosjr@live.com>
2022-04-24 18:30:30 -0300

    Use State

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,24 @@
 import './styles.css'
 import { Card } from '../../components/Card'
+import { useState } from 'react'
 
 export function Home() {
+  const [studentName, setStudentName] = useState("")
+
+
   return (
     <div className="container">
-    
      <h1>Lista de Presença</h1>
-     <input type="text" placeholder = "Digite seu nome..." />
+     <h3>Nome: {studentName}</h3>
+     <input 
+     type="text" 
+     placeholder = "Digite seu nome..."
+     onChange = {(e) => setStudentName(e.target.value)}
+     />
+     
      <button type='text'>Adicionar</button>
 
+    {/* Importantdo componente Card */}
      <Card name="Rodrigo" time="10:55:25" />
      <Card name="João" time="11:00:10" />
      <Card name="Ana" time="12:10:33" />

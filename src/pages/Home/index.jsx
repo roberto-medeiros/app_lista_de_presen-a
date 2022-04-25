442c9f921a93ffe0d1025203d71cd61db862890a
roberto-medeiros <robertomedeirosjr@live.com>
2022-04-24 21:24:43 -0300

    Consumindo APi do Github e useEffect Async

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
 
 import "./styles.css";
 import { Card } from "../../components/Card";
 
 export function Home() {
   const [studentName, setStudentName] = useState("");
-
   const [students, setStudents] = useState([]);
+  const [user, setUser] = useState({name:'', avatar:''});
 
   function handleAddStudent() {
     const newStudent = {
@@ -17,13 +18,51 @@ export function Home() {
         second: "2-digit",
       }),
     };
-
+   //prevState é para manter o valor anterior e o new, para adiconar um novo valor
     setStudents((prevState) => [...prevState, newStudent]);
   }
 
+  // useEffect(()=>{
+  //   //Corpo do useEffect
+  //   //Consumindo API do Github
+  //   fetch('https://api.github.com/users/roberto-medeiros')
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setUser({
+  //         name: data.name,
+  //         avatar: data.avatar_url,
+  //       });
+  //     });
+  //   console.log('O Use effect foi chamado')
+  // }, []);
+
+  //Exemplo como lidar com requisições assíncronas utilizando o Hook useEffect
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("https://api.github.com/users/roberto-medeiros");
+      const data = await response.json();
+      console.log("DADOS =>", data);
+
+      setUser({
+        name: data.name,
+        avatar: data.avatar_url,
+      });
+    }
+
+    fetchData();
+  }, []);
+
+
   return (
     <div className="container">
-      <h1>Lista de Presença</h1>
+      <header>
+        <h1>Lista de Presença</h1>
+        <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="Foto de perfil" />
+        </div>
+      </header>
+
       <input
         type="text"
         placeholder="Digite o nome..."
@@ -34,7 +73,11 @@ export function Home() {
       </button>
       {/* Importantdo componente Card */}
       {students.map((student) => (
-        <Card name={student.name} time={student.time} />
+        <Card 
+        key={student.time} 
+        name={student.name} 
+        time={student.time} 
+        />
       ))}
     </div>
   );
